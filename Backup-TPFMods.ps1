2aa5c46c17d80c1e31665d9c5ab178b22b5d7f70
Huxley Deng <dhao2001@outlook.com>
2022-07-27 00:39:08 +0800

    add: Some new function

--- Backup-TPFMods.ps1
+++ Backup-TPFMods.ps1
@@ -11,8 +11,15 @@ param (
 
 
 <#
+    Script scope variable used.
+#>
+$script:_use7zip = $false
+$script:_7zipPath = $null
+
+
+<#
     .DESCRIPTION
-    The Get-7ZipVersion function returns the version and architecture of 7z.exe execution,
+    The `Get-7ZipVersion` function returns the version and architecture of 7z.exe execution,
     which is given in SevenZipPath parameter.
 #>
 function Get-7ZipVersion {
@@ -31,7 +38,7 @@ function Get-7ZipVersion {
 
     $sevenZipMatch = $null
     try {
-        $sevenZipMatch = (& $SevenZipPath | Select-String -Pattern "7-Zip").Line -match '7-Zip (?<ver>\d+\.\d+) \((?<arch>\w+)\)'
+        $sevenZipMatch = (& $SevenZipPath | Select-String -Pattern "7-Zip").Line -match '7-Zip\s(?:\([az]\)\s)?(?<ver>\d+\.\d+)\s\((?<arch>\w+)\)'
     }
     catch [System.Management.Automation.CommandNotFoundException] {
         throw $_
@@ -47,13 +54,35 @@ function Get-7ZipVersion {
     return $result
 }
 
+<#
+    .DESCRIPTION
+    The `Use-7Zip‘ funcion sets the 7z.exe execution's path and flag in script scope.
+#>
+function Use-7Zip {
+    param (
+        [string]$SevenZipPath
+    )
+
+    $script:_use7zip = $true
+    $script:_7zipPath = $SevenZipPath
+}
 
 
+function New-7ZipArchive {
+    param (
+        
+    )
+
+    & $script:_7zipPath
+}
 
 
 
 function Backup-TPFMods {
-    
+    if ( -not [string]::IsNullOrWhiteSpace($script:Use7Zip)) {
+        $szVer = Get-7ZipVersion $script:Use7Zip
+    }
+    Write-Output $szVer
 }
 
 Backup-TPFMods
