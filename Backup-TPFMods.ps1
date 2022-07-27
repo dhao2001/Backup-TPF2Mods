da11d088749341358449ad91aaffb4c358517507
Huxley Deng <dhao2001@outlook.com>
2022-07-27 17:59:17 +0800

    add: Function For Detecting System-wide 7-Zip
    Add some functions to detect system platform and available 7-Zip
    executable on host.

--- Backup-TPFMods.ps1
+++ Backup-TPFMods.ps1
@@ -17,6 +17,32 @@ $script:_use7zip = $false
 $script:_7zipPath = $null
 
 
+
+<#
+    Utilities
+#>
+
+enum HostPlatform {
+    Windows
+    Unix
+}
+
+function Get-Platform {
+    if (!$PSVersionTable.Platform -or $PSVersionTable.Platform -eq 'Win32NT') {
+        return [HostPlatform]::Windows
+    }
+    elseif ($PSVersionTable.Platform -eq 'Unix') {
+        return [HostPlatform]::Unix
+    }
+}
+
+
+
+<#
+    Functions related to 7-Zip.
+#>
+
+
 <#
     .DESCRIPTION
     The `Get-7ZipVersion` function returns the version and architecture of 7z.exe execution,
@@ -54,6 +80,70 @@ function Get-7ZipVersion {
     return $result
 }
 
+
+
+
+<#
+    .DESCRIPTION
+    This `Get-System7ZipVersion` function searches 7-Zip executable 
+    in different locations by following order:
+
+    1. Directory where this script exists.
+    2. Directories in *PATH*
+    3. Directory where 7-Zip usually installed, especially on Windows.
+
+    This function searches for aliases to 7-Zip as follow:
+
+    - Windows: `7z` for installation version, and `7za` for 
+      standAlone version (according to 7-Zip official release).
+    - Linux: `7zz` provided by Igor Pavlov or package `7zip` on
+      Debian 12(bookworm) / Ubuntu 22.04 or later
+
+#>
+function Get-System7ZipVersion {
+    param (
+        [Parameter()]
+        [ValidateSet('Windows', 'Unix')]
+        [string]
+        $Platform
+    )
+
+    $search7zPath = $null
+    if ($Platform -eq 'Windows') {
+        $search7zPath = @(
+            Join-Path '.' '7za'
+            Join-Path '.' '7z'
+            '7za'
+            '7z'
+            Join-Path $env:ProgramFiles '7-Zip' '7z'
+            Join-Path ${env:ProgramFiles(x86)} '7-Zip' '7z'
+        )
+    }
+    elseif ($Platform -eq 'Unix') {
+        $search7zPath = @(
+            Join-Path '.' '7zz'
+            Join-Path '.' '7z'
+            '7zz'
+            '7z'
+            "/bin/7zz"
+            "/usr/bin/7zz"
+        )
+    }
+    foreach ($z in $search7zPath) {
+        if ($c = Get-Command $z -ErrorAction SilentlyContinue) {
+            $ver = Get-7ZipVersion -SevenZipPath $c.Path
+            if ($ver.Available) {
+                return $ver
+            }
+        }
+    }
+    return $ver
+}
+
+
+
+
+
 <#
     .DESCRIPTION
     The `Use-7Zip‘ funcion sets the 7z.exe execution's path and flag in script scope.
@@ -79,10 +169,19 @@ function New-7ZipArchive {
 
 
 function Backup-TPFMods {
-    if ( -not [string]::IsNullOrWhiteSpace($script:Use7Zip)) {
-        $szVer = Get-7ZipVersion $script:Use7Zip
+    $platform = Get-Platform
+    if (-not [string]::IsNullOrWhiteSpace($script:Use7Zip)) {
+        $7zver = Get-7ZipVersion $script:Use7Zip
+        if ($7zver -and $7zver.Available) {
+            Write-Output "Using User's 7-Zip $($7zver.Version) at $($7zver.Path)"
+        }
     }
-    Write-Output $szVer
+    elseif (($7zver = Get-System7ZipVersion $platform) -and $7zver.Available) {
+        Write-Output "Using System 7-Zip $($7zver.Version) at $($7zver.Path)"
+    }
+    else {
+        Write-Output "No available 7-Zip"
+    }
 }
 
 Backup-TPFMods
